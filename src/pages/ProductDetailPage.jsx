1753ee6071e84a2e5a38c90ef13f31aef21887e9
Pahmiii <agilfahmirakasaputra073@gmail.com>
2025-10-11 10:59:44 +0700

    feat: updating product detail page

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -1,22 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { getProductById, addProductToCart } from '../services/api';
+import { getProductById, getCategoryById, addProductToCart } from '../services/api';
 
 const ProductDetailPage = () => {
   const [product, setProduct] = useState(null);
+  const [category, setCategory] = useState("");
+  const [desc, setDesc] = useState(true);
   const [quantity, setQuantity] = useState(1);
+  const [selectedSize, setSelectedSize] = useState("");
   const { id } = useParams();
 
   useEffect(() => {
-    const fetchProduct = async () => {
+    const fetchData = async () => {
       try {
-        const response = await getProductById(id);
-        setProduct(response.data.data);
+        const productResponse = await getProductById(id);
+        const fetchedProduct = productResponse.data.data;
+        setProduct(fetchedProduct);
+
+        const categoryResponse = await getCategoryById(fetchedProduct.categoryId);
+        setCategory(categoryResponse.data.data.name);
       } catch (error) {
-        console.error('Error fetching product:', error);
+        console.error('Error fetching data:', error);
       }
     };
-    fetchProduct();
+    fetchData();
   }, [id]);
 
   const handleAddToCart = async () => {
@@ -32,7 +39,7 @@ const ProductDetailPage = () => {
   }
 
   return (
-    <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+    <div className="container mx-auto px-4 sm:px-6 lg:px-8 text-primary">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
         <div>
           <img
@@ -41,29 +48,87 @@ const ProductDetailPage = () => {
             alt={product.name}
           />
         </div>
-        <div>
-          <h1 className="text-3xl font-bold mb-4 text-gray-800 uppercase">{product.name}</h1>
-          <p className="text-gray-800 font-semibold text-xl mb-4">${product.price}</p>
-          <h1 className="text-2xl font-bold mb-4 text-gray-800 uppercase">Product Description</h1>
-          <p className="text-gray-600 mb-6">{product.description}</p>
-          <div className="flex items-center mb-6">
-            <label htmlFor="quantity" className="mr-4 text-gray-700 font-bold uppercase">Quantity:</label>
-            <input
-              type="number"
-              id="quantity"
-              name="quantity"
-              min="1"
-              value={quantity}
-              onChange={(e) => setQuantity(parseInt(e.target.value))}
-              className="shadow appearance-none border w-20 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            />
+        <div className="flex flex-col space-y-5">
+          <div>
+            <h1 className="text-3xl font-bold mb-4 uppercase">{product.name}</h1>
+            <p className="text-gray-800 font-semibold text-2xl">${product.price}</p>
           </div>
-          <button
-            onClick={handleAddToCart}
-            className="bg-accent hover:bg-red-700 text-white font-bold uppercase py-2 px-4 focus:outline-none focus:shadow-outline w-full"
-          >
-            Add to Cart
-          </button>
+          <div className="border-t-2 border-primary mb-2 w-full"/>
+          <div>
+            <div className="flex justify-between">
+              <h3 className="text-xl font-bold mb-2 uppercase">Product Description</h3>
+              <button
+                  onClick={() => setDesc(!desc)}
+                  className="px-4 py-2 text-xl font-bold hover:bg-gray-100"
+                >
+                  -
+                </button>
+            </div>
+            { desc ? ( <p className="text-gray-700">{product.description}</p> ) : ( <></> )}
+          </div>
+          { category === "Clothing" ? (
+            <>
+              <div className="border-t-2 border-primary mb-2 w-full"/>
+              <div>
+                <h3 className="text-xl font-bold mb-2 uppercase">Size</h3>
+                <select
+                  value={selectedSize}
+                  onChange={(e) => setSelectedSize(e.target.value)}
+                  className="w-full border-2 border-primary py-2 px-3 text-gray-700"
+                >
+                  <option value="">Choose an option</option>
+                  <option value="S">Small</option>
+                  <option value="M">Medium</option>
+                  <option value="L">Large</option>
+                </select>
+              </div>
+            </>
+            ) : category === "Books" ? (
+              <></>
+            ) : ( <></> )
+          }
+          <div className="border-t-2 border-primary mb-2 w-full"/>
+          <div>
+            <h3 className="text-xl font-bold mb-2 uppercase">Quantity</h3>
+            <div className="flex items-center w-fit h-12 space-x-1">
+              <div className="h-full border-2 border-primary">
+                <button
+                  onClick={() => setQuantity((q) => Math.max(1, q - 1))}
+                  className={`w-12 px-4 py-2 text-xl font-bold transition 
+                  ${
+                    quantity === 1
+                      ? "text-gray-300 cursor-not-allowed"
+                      : "text-gray-700 hover:bg-gray-100"
+                  }`}
+                >
+                  -
+                </button>
+              </div>
+              <div className="flex items-center justify-center h-full w-20 border-2 border-primary">
+                <p className="px-6 text-gray-700">{quantity}</p>
+              </div>
+              <div className="h-full border-2 border-primary">
+                <button
+                  onClick={() => setQuantity((q) => q + 1)}
+                  className="w-12 px-4 py-2 text-xl font-bold text-gray-700 hover:bg-gray-100"
+                >
+                  +
+                </button>
+              </div>
+            </div>
+          </div>
+          <div>
+            <button
+              onClick={handleAddToCart}
+              className="w-full py-2 px-4 font-bold uppercase bg-accent hover:bg-red-700 text-secondary focus:outline-none focus:shadow-outline"
+            >
+              Add to Cart
+            </button>
+          </div>
+          <div className="flex justify-between text-gray-700">
+            <button className="hover:underline">Add to Wishlist</button>
+            <button className="hover:underline">Share</button>
+          </div>
         </div>
       </div>
     </div>
